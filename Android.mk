cfa35d5b5092caf18771b384f954e8001937952d
Guang Zhu <guangzhu@google.com>
2010-04-01 18:13:01 -0700

    add launch performance test for deskclock
    this is part of a larger effort to collect app launch performance for the platform.
    
    Change-Id: I23974688bad1f66aa24906c73317c6ace67c41fd

--- Android.mk
+++ Android.mk
@@ -3,7 +3,7 @@ include $(CLEAR_VARS)
 
 LOCAL_MODULE_TAGS := optional
 
-LOCAL_SRC_FILES := $(call all-subdir-java-files)
+LOCAL_SRC_FILES := $(call all-java-files-under, src)
 
 LOCAL_PACKAGE_NAME := DeskClock
 
@@ -12,3 +12,6 @@ LOCAL_OVERRIDES_PACKAGES := AlarmClock
 LOCAL_SDK_VERSION := current
 
 include $(BUILD_PACKAGE)
+
+# Use the following include to make our test apk.
+include $(call all-makefiles-under,$(LOCAL_PATH))
