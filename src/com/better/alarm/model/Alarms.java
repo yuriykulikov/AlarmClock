4083b688be12c27c4a2b1b4218b9e4258329e352
Yuriy Kulikov <yuriy.kulikov.87@googlemail.com>
2012-07-14 21:42:00 +0200

    Removed snooze functionality

--- src/com/better/alarm/model/Alarms.java
+++ src/com/better/alarm/model/Alarms.java
@@ -19,7 +19,6 @@ package com.better.alarm.model;
 import java.util.HashSet;
 import java.util.Set;
 
-import android.app.NotificationManager;
 import android.content.ContentResolver;
 import android.content.ContentUris;
 import android.content.ContentValues;
@@ -28,11 +27,8 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.database.Cursor;
 import android.net.Uri;
-import android.preference.PreferenceManager;
-import android.util.Log;
 
 import com.better.alarm.presenter.AlarmsListActivity;
-import com.better.alarm.presenter.SettingsActivity;
 
 /**
  * The Alarms provider supplies info about Alarm Clock settings
@@ -96,7 +92,7 @@ public class Alarms implements IAlarmsManager, IAlarmsSetup {
 
         long timeInMillis = alarm.getTimeInMillis();
         if (alarm.enabled) {
-            clearSnoozeIfNeeded(timeInMillis);
+            //clearSnoozeIfNeeded(timeInMillis);
         }
         setNextAlert();
         return timeInMillis;
@@ -111,11 +107,10 @@ public class Alarms implements IAlarmsManager, IAlarmsSetup {
         if (alarmId == INVALID_ALARM_ID) return;
 
         ContentResolver contentResolver = mContext.getContentResolver();
-        /* If alarm is snoozing, lose it */
-        disableSnoozeAlert(alarmId);
-
         Uri uri = ContentUris.withAppendedId(Alarm.Columns.CONTENT_URI, alarmId);
         contentResolver.delete(uri, "", null);
+
+        broadcastAlarmState(getAlarm(alarmId), Intents.ALARM_DISMISS_ACTION);
 
         setNextAlert();
     }
@@ -140,8 +135,6 @@ public class Alarms implements IAlarmsManager, IAlarmsSetup {
     void onAlarmFired(Alarm alarm) {
         broadcastAlarmState(alarm, Intents.ALARM_ALERT_ACTION);
 
-        // Disable the snooze alert if this alarm is the snooze.
-        disableSnoozeAlert(alarm.id);
         // Disable this alarm if it does not repeat.
         if (!alarm.daysOfWeek.isRepeatSet()) {
             enableAlarmInternal(alarm, false);
@@ -181,22 +174,6 @@ public class Alarms implements IAlarmsManager, IAlarmsSetup {
         return values;
     }
 
-    private void clearSnoozeIfNeeded(long alarmTime) {
-        // If this alarm fires before the next snooze, clear the snooze to
-        // enable this alarm.
-        SharedPreferences prefs = mContext.getSharedPreferences(AlarmsListActivity.PREFERENCES, 0);
-
-        // Get the list of snoozed alarms
-        final Set<String> snoozedIds = prefs.getStringSet(PREF_SNOOZE_IDS, new HashSet<String>());
-        for (String snoozedAlarm : snoozedIds) {
-            final long snoozeTime = prefs.getLong(getAlarmPrefSnoozeTimeKey(snoozedAlarm), 0);
-            if (alarmTime < snoozeTime) {
-                final int alarmId = Integer.parseInt(snoozedAlarm);
-                clearSnoozePreference(prefs, alarmId);
-            }
-        }
-    }
-
     /**
      * Return an Alarm object representing the alarm id in the database. Returns
      * null if no alarm exists.
@@ -233,12 +210,10 @@ public class Alarms implements IAlarmsManager, IAlarmsSetup {
             // only does work if the snoozed alarm is the same as the given
             // alarm.
             // TODO: disableSnoozeAlert should have a better name.
-            disableSnoozeAlert(alarm.id);
 
             // Disable the snooze if this alarm fires before the snoozed alarm.
             // This works on every alarm since the user most likely intends to
             // have the modified alarm fire next.
-            clearSnoozeIfNeeded(timeInMillis);
         }
 
         setNextAlert();
@@ -262,12 +237,7 @@ public class Alarms implements IAlarmsManager, IAlarmsSetup {
 
     @Override
     public void snooze(Alarm alarm) {
-        final String snooze = PreferenceManager.getDefaultSharedPreferences(mContext).getString(
-                SettingsActivity.KEY_ALARM_SNOOZE, DEFAULT_SNOOZE);
-        int snoozeMinutes = Integer.parseInt(snooze);
-
-        final long snoozeTime = System.currentTimeMillis() + (1000 * 60 * snoozeMinutes);
-        saveSnoozeAlert(alarm, snoozeTime);
+        // TODO
     }
 
     @Override
@@ -296,7 +266,6 @@ public class Alarms implements IAlarmsManager, IAlarmsSetup {
             values.put(Alarm.Columns.ALARM_TIME, time);
         } else {
             // Clear the snooze if the id matches.
-            disableSnoozeAlert(alarm.id);
         }
 
         resolver.update(ContentUris.withAppendedId(Alarm.Columns.CONTENT_URI, alarm.id), values, null, null);
@@ -305,7 +274,6 @@ public class Alarms implements IAlarmsManager, IAlarmsSetup {
     private Alarm calculateNextAlert() {
         long minTime = Long.MAX_VALUE;
         long now = System.currentTimeMillis();
-        final SharedPreferences prefs = mContext.getSharedPreferences(AlarmsListActivity.PREFERENCES, 0);
 
         Set<Alarm> alarms = new HashSet<Alarm>();
 
@@ -314,14 +282,6 @@ public class Alarms implements IAlarmsManager, IAlarmsSetup {
         // list. For a non-repeating alarm, when it goes of, it becomes
         // disabled. A snoozed
         // non-repeating alarm is not in the active list in the database.
-
-        // first go through the snoozed alarms
-        final Set<String> snoozedIds = prefs.getStringSet(PREF_SNOOZE_IDS, new HashSet<String>());
-        for (String snoozedAlarm : snoozedIds) {
-            final int alarmId = Integer.parseInt(snoozedAlarm);
-            final Alarm a = getAlarm(alarmId);
-            alarms.add(a);
-        }
 
         // Now add the scheduled alarms
         final Cursor cursor = getFilteredAlarmsCursor(mContext.getContentResolver());
@@ -346,9 +306,6 @@ public class Alarms implements IAlarmsManager, IAlarmsSetup {
             if (a.time == 0) {
                 a.time = a.getTimeInMillis();
             }
-
-            // Update the alarm if it has been snoozed
-            updateAlarmTimeForSnooze(prefs, a);
 
             if (a.time < now) {
                 // Expired alarm, disable it and move along.
@@ -401,99 +358,6 @@ public class Alarms implements IAlarmsManager, IAlarmsSetup {
             AlarmReceiver.removeRTCAlarm(mContext);
             broadcastAlarmState(alarm, Intents.ACTION_ALARMS_UNSCHEDULED);
         }
-    }
-
-    private void saveSnoozeAlert(Alarm alarm, long time) {
-        SharedPreferences prefs = mContext.getSharedPreferences(AlarmsListActivity.PREFERENCES, 0);
-        if (alarm.id == INVALID_ALARM_ID) {
-            clearAllSnoozePreferences(prefs);
-        } else {
-            final Set<String> snoozedIds = prefs.getStringSet(PREF_SNOOZE_IDS, new HashSet<String>());
-            snoozedIds.add(Integer.toString(alarm.id));
-            final SharedPreferences.Editor ed = prefs.edit();
-            ed.putStringSet(PREF_SNOOZE_IDS, snoozedIds);
-            ed.putLong(getAlarmPrefSnoozeTimeKey(alarm.id), time);
-            ed.apply();
-        }
-        // Set the next alert after updating the snooze.
-        setNextAlert();
-    }
-
-    private String getAlarmPrefSnoozeTimeKey(int id) {
-        return getAlarmPrefSnoozeTimeKey(Integer.toString(id));
-    }
-
-    private String getAlarmPrefSnoozeTimeKey(String id) {
-        return PREF_SNOOZE_TIME + id;
-    }
-
-    /**
-     * Disable the snooze alert if the given id matches the snooze id.
-     */
-    private void disableSnoozeAlert(final int id) {
-        SharedPreferences prefs = mContext.getSharedPreferences(AlarmsListActivity.PREFERENCES, 0);
-        if (hasAlarmBeenSnoozed(prefs, id)) {
-            // This is the same id so clear the shared prefs.
-            clearSnoozePreference(prefs, id);
-        }
-    }
-
-    // XXX Helper to remove the snooze preference. Do not use clear because that
-    // will erase the clock preferences. Also clear the snooze notification in
-    // the window shade.
-    private void clearSnoozePreference(final SharedPreferences prefs, final int id) {
-        final String alarmStr = Integer.toString(id);
-        final Set<String> snoozedIds = prefs.getStringSet(PREF_SNOOZE_IDS, new HashSet<String>());
-        if (snoozedIds.contains(alarmStr)) {
-            NotificationManager nm = (NotificationManager) mContext.getSystemService(Context.NOTIFICATION_SERVICE);
-            nm.cancel(id);
-        }
-
-        final SharedPreferences.Editor ed = prefs.edit();
-        snoozedIds.remove(alarmStr);
-        ed.putStringSet(PREF_SNOOZE_IDS, snoozedIds);
-        ed.remove(getAlarmPrefSnoozeTimeKey(alarmStr));
-        ed.apply();
-    }
-
-    // XXX
-    private void clearAllSnoozePreferences(final SharedPreferences prefs) {
-        NotificationManager nm = (NotificationManager) mContext.getSystemService(Context.NOTIFICATION_SERVICE);
-        final Set<String> snoozedIds = prefs.getStringSet(PREF_SNOOZE_IDS, new HashSet<String>());
-        final SharedPreferences.Editor ed = prefs.edit();
-        for (String snoozeId : snoozedIds) {
-            nm.cancel(Integer.parseInt(snoozeId));
-            ed.remove(getAlarmPrefSnoozeTimeKey(snoozeId));
-        }
-
-        ed.remove(PREF_SNOOZE_IDS);
-        ed.apply();
-    }
-
-    private boolean hasAlarmBeenSnoozed(final SharedPreferences prefs, final int alarmId) {
-        final Set<String> snoozedIds = prefs.getStringSet(PREF_SNOOZE_IDS, null);
-
-        // Return true if there a valid snoozed alarmId was saved
-        return snoozedIds != null && snoozedIds.contains(Integer.toString(alarmId));
-    }
-
-    /**
-     * Updates the specified Alarm with the additional snooze time. Returns a
-     * boolean indicating whether the alarm was updated.
-     */
-    private boolean updateAlarmTimeForSnooze(final SharedPreferences prefs, final Alarm alarm) {
-        if (!hasAlarmBeenSnoozed(prefs, alarm.id)) {
-            // No need to modify the alarm
-            return false;
-        }
-
-        final long time = prefs.getLong(getAlarmPrefSnoozeTimeKey(alarm.id), -1);
-        // The time in the database is either 0 (repeating) or a specific time
-        // for a non-repeating alarm. Update this value so the AlarmReceiver
-        // has the right time to compare.
-        alarm.time = time;
-
-        return true;
     }
 
     private void broadcastAlarmState(Alarm alarm, String action) {
