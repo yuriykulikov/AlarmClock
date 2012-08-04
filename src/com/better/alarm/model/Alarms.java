yuriykulikov/AlarmClock

commit 49d5841f04025e5a481117e4100e946e0666b331

Fixed #6 : use getNextTimeInMillis in init()
we can determine which alarms have expired using time value stored
in the database.

	modified:   src/com/better/alarm/model/Alarm.java
	modified:   src/com/better/alarm/model/Alarms.java

src/com/better/alarm/model/Alarms.java
@@ -75,7 +75,14 @@ public class Alarms implements IAlarmsManager {
     }
 
     void init() {
-        disableExpiredAlarms();
+        // disable expired alarms
+        long now = System.currentTimeMillis();
+        for (Alarm alarm : set) {
+            boolean isExpired = alarm.getNextTimeInMillis() < now;
+            if (isExpired && !alarm.isEnabled()) {
+                enable(alarm.getId(), false);
+            }
+        }
         setNextAlert();
     }
 
@@ -230,18 +237,6 @@ public class Alarms implements IAlarmsManager {
             }
         }
         return alarm;
-    }
-
-    /**
-     * Disables non-repeating alarms that have passed. Called at boot.
-     */
-    void disableExpiredAlarms() {
-        long now = System.currentTimeMillis();
-        for (Alarm alarm : set) {
-            if (!alarm.isEnabled() && alarm.getTimeInMillis() != 0 && alarm.getTimeInMillis() < now) {
-                enable(alarm.getId(), false);
-            }
-        }
     }
 
     /**

src/com/better/alarm/model/Alarm.java
@@ -158,7 +158,6 @@ public final class Alarm implements Comparable<Alarm> {
         public static final int ALARM_HOUR_INDEX = 1;
         public static final int ALARM_MINUTES_INDEX = 2;
         public static final int ALARM_DAYS_OF_WEEK_INDEX = 3;
-        @Deprecated
         public static final int ALARM_TIME_INDEX = 4;
         public static final int ALARM_ENABLED_INDEX = 5;
         public static final int ALARM_VIBRATE_INDEX = 6;
@@ -182,6 +181,12 @@ public final class Alarm implements Comparable<Alarm> {
     private boolean enabled;
     private int hour;
     private int minutes;
+    /**
+     * Time when Alarm would normally go next time. Used to disable expired
+     * alarms if devicee was off-line when they were supposed to fire
+     * 
+     */
+    private long nextTimeInMillis;
     private DaysOfWeek daysOfWeek;
     private boolean vibrate;
     private String label;
@@ -197,6 +202,7 @@ public final class Alarm implements Comparable<Alarm> {
         enabled = c.getInt(Columns.ALARM_ENABLED_INDEX) == 1;
         hour = c.getInt(Columns.ALARM_HOUR_INDEX);
         minutes = c.getInt(Columns.ALARM_MINUTES_INDEX);
+        nextTimeInMillis = (c.getLong(Columns.ALARM_TIME_INDEX));
         daysOfWeek = new DaysOfWeek(c.getInt(Columns.ALARM_DAYS_OF_WEEK_INDEX));
         vibrate = c.getInt(Columns.ALARM_VIBRATE_INDEX) == 1;
         label = c.getString(Columns.ALARM_MESSAGE_INDEX);
@@ -230,6 +236,7 @@ public final class Alarm implements Comparable<Alarm> {
         minutes = c.get(Calendar.MINUTE);
         vibrate = true;
         daysOfWeek = new DaysOfWeek(0);
+        nextTimeInMillis = c.getTimeInMillis();
         alert = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_ALARM);
         prealarm = false;
         snoozed = false;
@@ -445,6 +452,14 @@ public final class Alarm implements Comparable<Alarm> {
         return vibrate;
     }
 
+    /**
+     * @see {@link #nextTimeInMillis}
+     * @return
+     */
+    long getNextTimeInMillis() {
+        return nextTimeInMillis;
+    }
+
     public DaysOfWeek getDaysOfWeek() {
         return daysOfWeek;
     }
@@ -471,12 +486,9 @@ public final class Alarm implements Comparable<Alarm> {
 
     ContentValues createContentValues() {
         ContentValues values = new ContentValues(8);
-        // Set the alarm_time value if this alarm does not repeat. This will be
-        // used later to disable expire alarms.
-        long time = 0;
-        if (!daysOfWeek.isRepeatSet()) {
-            time = getTimeInMillis();
-        }
+        // Set the alarm_time value. This will be
+        // used later to disable expired alarms.
+        long time = getTimeInMillis();
 
         values.put(Alarm.Columns.ENABLED, enabled ? 1 : 0);
         values.put(Alarm.Columns.HOUR, hour);
@@ -535,6 +547,10 @@ public final class Alarm implements Comparable<Alarm> {
 
     void setVibrate(boolean vibrate) {
         this.vibrate = vibrate;
+    }
+
+    void setNextTimeInMillis(int nextTimeInMillis) {
+        this.nextTimeInMillis = nextTimeInMillis;
     }
 
     void setDaysOfWeek(DaysOfWeek daysOfWeek) {

commit fc1d53fff802d8e89234845fff8928b3bdd8a2bd

Alarms: small refactoring
this fixes issue introduced by 49d5841f04025e5a481117e4100e946e0666b331
which was supposed for issue #6

src/com/better/alarm/model/Alarms.java
@@ -80,10 +80,24 @@ public class Alarms implements IAlarmsManager {
         for (Alarm alarm : set) {
             boolean isExpired = alarm.getNextTimeInMillis() < now;
             if (isExpired && !alarm.isEnabled()) {
-                enable(alarm.getId(), false);
+                alarm.setEnabled(false);
+                writeToDb(alarm.getId());
             }
         }
         setNextAlert();
+    }
+
+    private void writeToDb(int id) {
+        ContentValues values = getAlarm(id).createContentValues();
+        Uri uriWithAppendedId = ContentUris.withAppendedId(Alarm.Columns.CONTENT_URI, id);
+        mContentResolver.update(uriWithAppendedId, values, null, null);
+    }
+
+    private void deleteAlarm(int alarmId) {
+        Alarm alarm = getAlarm(alarmId);
+        set.remove(alarm);
+        Uri uri = ContentUris.withAppendedId(Alarm.Columns.CONTENT_URI, alarmId);
+        mContentResolver.delete(uri, "", null);
     }
 
     @Override
@@ -109,19 +123,16 @@ public class Alarms implements IAlarmsManager {
         alarm.setLabel(label);
         alarm.setAlert(alert);
         alarm.setPrealarm(preAlarm);
-        changeAlarm(alarm);
+        writeToDb(alarm.getId());
+        setNextAlert();
+        notifyAlarmListChangedListeners();
     }
 
     @Override
     public void delete(int alarmId) {
-        Alarm alarm = getAlarm(alarmId);
-        set.remove(alarm);
-        Uri uri = ContentUris.withAppendedId(Alarm.Columns.CONTENT_URI, alarmId);
-        mContentResolver.delete(uri, "", null);
-
+        deleteAlarm(alarmId);
         broadcastAlarmState(alarmId, Intents.ALARM_DISMISS_ACTION);
         notifyAlarmListChangedListeners();
-
         setNextAlert();
     }
 
@@ -139,7 +150,9 @@ public class Alarms implements IAlarmsManager {
         if (!alarm.getDaysOfWeek().isRepeatSet()) {
             alarm.setEnabled(false);
         }
-        changeAlarm(alarm);
+        writeToDb(alarm.getId());
+        setNextAlert();
+        notifyAlarmListChangedListeners();
     }
 
     void onAlarmSnoozedFired(int id) {
@@ -161,32 +174,6 @@ public class Alarms implements IAlarmsManager {
         return null;
     }
 
-    private int changeAlarm(Alarm alarm) {
-        // first add to the DB
-        ContentValues values = alarm.createContentValues();
-
-        // this is an existing alarm
-        mContentResolver.update(ContentUris.withAppendedId(Alarm.Columns.CONTENT_URI, alarm.getId()), values, null,
-                null);
-        set.remove(alarm);
-        set.add(alarm);
-
-        if (alarm.isEnabled()) {
-            // Disable the snooze if we just changed the snoozed alarm. This
-            // only does work if the snoozed alarm is the same as the given
-            // alarm.
-            // TODO: disableSnoozeAlert should have a better name.
-
-            // Disable the snooze if this alarm fires before the snoozed alarm.
-            // This works on every alarm since the user most likely intends to
-            // have the modified alarm fire next.
-            // clearSnoozeIfNeeded(timeInMillis);
-        }
-        setNextAlert();
-        notifyAlarmListChangedListeners();
-        return alarm.getId();
-    }
-
     /**
      * A convenience method to enable or disable an alarm.
      * 
@@ -199,7 +186,9 @@ public class Alarms implements IAlarmsManager {
     public void enable(int id, boolean enable) {
         Alarm alarm = getAlarm(id);
         alarm.setEnabled(enable);
-        changeAlarm(alarm);
+        writeToDb(alarm.getId());
+        setNextAlert();
+        notifyAlarmListChangedListeners();
     }
 
     @Override
@@ -212,14 +201,18 @@ public class Alarms implements IAlarmsManager {
         alarm.setSnoozedHour(calendar.get(Calendar.HOUR_OF_DAY));
         alarm.setSnoozedminutes(calendar.get(Calendar.MINUTE));
         broadcastAlarmState(alarm.getId(), Intents.ALARM_SNOOZE_ACTION);
-        changeAlarm(alarm);
+        writeToDb(alarm.getId());
+        setNextAlert();
+        notifyAlarmListChangedListeners();
     }
 
     @Override
     public void dismiss(Alarm alarm) {
         broadcastAlarmState(alarm.getId(), Intents.ALARM_DISMISS_ACTION);
         alarm.setSnoozed(false);
-        changeAlarm(alarm);
+        writeToDb(alarm.getId());
+        setNextAlert();
+        notifyAlarmListChangedListeners();
     }
 
     private Alarm calculateNextAlert() {
@@ -244,7 +237,7 @@ public class Alarms implements IAlarmsManager {
      * changes alarm settings. Activates snooze if set, otherwise loads all
      * alarms, activates next alert.
      */
-    void setNextAlert() {
+    private void setNextAlert() {
         final Alarm alarm = calculateNextAlert();
         if (alarm != null) {
             long timeInMillis = alarm.isSnoozed() ? alarm.getSnoozedTimeInMillis() : alarm.getTimeInMillis();