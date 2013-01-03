e2f7bd076113c152d567d3e800f3bc85d6bfe359
Yuriy Kulikov <yuriy.kulikov.87@gmail.com>
2013-01-03 23:39:01 +0100

    remove old stuff from DB

--- src/com/better/alarm/model/AlarmDatabaseHelper.java
+++ src/com/better/alarm/model/AlarmDatabaseHelper.java
@@ -55,16 +55,13 @@ class AlarmDatabaseHelper extends SQLiteOpenHelper {
                 "message TEXT, " +
                 "alert TEXT, " +
                 "prealarm INTEGER, " +
-                "prealarm_time INTEGER, " +
-                "snoozed INTEGER, " +
-                "snooze_time INTEGER, " +
                 "state STRING);");
         // @formatter:on
         // insert default alarms
         String insertMe = "INSERT INTO alarms " + "(hour, minutes, daysofweek, alarmtime, enabled, vibrate, "
-                + "message, alert, prealarm, prealarm_time, snoozed, snooze_time) VALUES ";
-        db.execSQL(insertMe + "(8, 30, 31, 0, 0, 1, '', '', 0, 0, 0, 0, '');");
-        db.execSQL(insertMe + "(9, 00, 96, 0, 0, 1, '', '', 0, 0, 0, 0, '');");
+                + "message, alert, prealarm, state) VALUES ";
+        db.execSQL(insertMe + "(8, 30, 31, 0, 0, 1, '', '', 0, '');");
+        db.execSQL(insertMe + "(9, 00, 96, 0, 0, 1, '', '', 0, '');");
     }
 
     @Override
