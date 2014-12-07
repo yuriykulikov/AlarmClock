8081ab69ea8ce28ba44e120a834f7feaf2221e0d
Yuriy Kulikov <yuriy.kulikov.87@gmail.com>
2014-12-07 18:43:14 +0100

    Use normal time (not smart rise time) in keyguard screen
    #ALARMCLOCK-58 fixed

--- src/com/better/alarm/presenter/background/ScheduledReceiver.java
+++ src/com/better/alarm/presenter/background/ScheduledReceiver.java
@@ -55,7 +55,9 @@ public class ScheduledReceiver extends BroadcastReceiver {
                 // KeyGuard)
                 // will react accordingly
                 String format = android.text.format.DateFormat.is24HourFormat(context) ? DM24 : DM12;
-                Calendar calendar = alarm.isSnoozed() ? alarm.getSnoozedTime() : alarm.getNextTime();
+                Calendar calendar = Calendar.getInstance();
+                long milliseconds = intent.getLongExtra(Intents.EXTRA_NEXT_NORMAL_TIME_IN_MILLIS, -1);
+                calendar.setTimeInMillis(milliseconds);
                 String timeString = (String) DateFormat.format(format, calendar);
                 Settings.System.putString(context.getContentResolver(), Settings.System.NEXT_ALARM_FORMATTED,
                         timeString);
