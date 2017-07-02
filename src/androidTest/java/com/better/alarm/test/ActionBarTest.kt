f7cc21ddeab420c8dd202b0b203c044f815fc4ca
Yuriy Kulikov <yuriy.kulikov.87@gmail.com>
2017-07-02 15:37:56 +0200

    Added Espresso tests

---
+++ src/androidTest/java/com/better/alarm/test/ActionBarTest.kt
@@ -0,0 +1,59 @@
+package com.better.alarm.test
+
+import android.app.Instrumentation
+import android.support.test.InstrumentationRegistry
+import android.support.test.rule.ActivityTestRule
+import android.support.test.runner.AndroidJUnit4
+import android.test.ActivityInstrumentationTestCase2
+import android.test.suitebuilder.annotation.LargeTest
+import com.better.alarm.logger.Logger
+import com.better.alarm.persistance.AlarmDatabaseHelper
+import com.better.alarm.presenter.AlarmsListActivity
+import com.robotium.solo.Solo
+import junit.framework.Assert
+import org.junit.*
+import org.junit.rules.RuleChain
+import org.junit.rules.TestRule
+import org.junit.runner.RunWith
+import java.util.Locale
+
+@RunWith(AndroidJUnit4::class)
+@LargeTest
+class ActionBarTest {
+    var listActivity = ActivityTestRule(
+            AlarmsListActivity::class.java, false, /* autostart*/ true)
+
+    @JvmField
+    @Rule
+    public var chain: TestRule = RuleChain.outerRule(ForceLocaleRule(Locale.US)).around(listActivity)
+
+    private var solo: Solo? = null;
+
+    companion object {
+        @JvmStatic
+        @BeforeClass
+        @AfterClass
+        public fun dropDatabase() {
+            val context = InstrumentationRegistry.getTargetContext()
+            val dbHelper = AlarmDatabaseHelper(context, Logger.create())
+            val db = dbHelper.writableDatabase
+            db.execSQL("DROP TABLE IF EXISTS alarms")
+            dbHelper.onCreate(db)
+            db.close()
+            println("Dropped database")
+        }
+    }
+
+    @Before public fun setup() {
+        solo = Solo(InstrumentationRegistry.getInstrumentation(), listActivity.activity)
+    }
+
+    @Test
+    fun testBugreportButton() {
+        val solo = solo!!
+        solo.sendKey(Solo.MENU)
+        solo.clickOnText("bugreport")
+        Assert.assertTrue(solo.searchText("Describe"))
+        solo.clickOnButton("Cancel")
+    }
+}
---
+++ src/androidTest/java/com/better/alarm/test/ForceLocaleRule.java
@@ -0,0 +1,54 @@
+package com.better.alarm.test;
+
+import android.content.res.Configuration;
+import android.content.res.Resources;
+import android.support.test.InstrumentationRegistry;
+
+import org.junit.rules.TestRule;
+import org.junit.runner.Description;
+import org.junit.runners.model.Statement;
+
+import java.util.Locale;
+
+/**
+ * Created by Yuriy on 01.07.2017.
+ */
+public class ForceLocaleRule implements TestRule {
+
+    private final Locale mTestLocale;
+    private Locale mDeviceLocale;
+
+    public ForceLocaleRule(Locale testLocale) {
+        mTestLocale = testLocale;
+    }
+
+    @Override
+    public Statement apply(final Statement base, Description description) {
+        return new Statement() {
+            public void evaluate() throws Throwable {
+                try {
+                    System.out.println("Setting locale to " + mTestLocale);
+                    if (mTestLocale != null) {
+                        mDeviceLocale = Locale.getDefault();
+                        setLocale(mTestLocale);
+                    }
+
+                    base.evaluate();
+                } finally {
+                    if (mDeviceLocale != null) {
+                        setLocale(mDeviceLocale);
+                    }
+                }
+            }
+        };
+    }
+
+
+    public void setLocale(Locale locale) {
+        Resources resources = InstrumentationRegistry.getTargetContext().getResources();
+        Locale.setDefault(locale);
+        Configuration config = resources.getConfiguration();
+        config.locale = locale;
+        resources.updateConfiguration(config, resources.getDisplayMetrics());
+    }
+}
