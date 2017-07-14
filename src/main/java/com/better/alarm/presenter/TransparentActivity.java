58b3668964e07778326e13ae6a8f245cd1fefdd4
Yuriy Kulikov <yuriy.kulikov.87@gmail.com>
2017-07-15 00:30:43 +0200

    Added a test for snooze from notification
    Added snoozeViaLongClick and snoozeViaNotification tests

--- src/main/java/com/better/alarm/presenter/TransparentActivity.java
+++ src/main/java/com/better/alarm/presenter/TransparentActivity.java
@@ -21,33 +21,15 @@ public class TransparentActivity extends Activity implements AlarmTimePickerDial
     protected void onCreate(Bundle icicle) {
         super.onCreate(icicle);
         IAlarmsManager alarmsManager = AlarmApplication.alarms();
-        Logger log = Logger.getDefaultLogger();
         Intent intent = getIntent();
-        log.d("Intent in TransparentActivity was received");
         int id = intent.getIntExtra(Intents.EXTRA_ID, -1);
-        try {
-            alarm = alarmsManager.getAlarm(id);
-            log.d("Alarm, that has to be rescheduled:" + alarm.toString());
-        } catch (Exception e) {
-            // TODO Auto-generated catch block
-            e.printStackTrace();
-        }
+        alarm = alarmsManager.getAlarm(id);
         TimePickerDialogFragment.showTimePicker(getFragmentManager());
-        log.d("Do you see time picker?");
-    }
-
-    @Override
-    public void onDestroy() {
-        super.onDestroy();
-        Logger.getDefaultLogger().d("TransparentActivity.onDestroy()");
-        // No longer care about the alarm being killed.
-
     }
 
     @Override
     public void onTimePickerCanceled() {
         finish();
-
     }
 
     @Override
