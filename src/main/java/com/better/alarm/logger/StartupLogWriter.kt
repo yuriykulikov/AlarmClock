e470b6163912bb85d8e36881c1b61d0c5eb1ee2c
Yuriy Kulikov <yuriy.kulikov.87@gmail.com>
2019-02-21 23:55:03 +0100

    Rewrite logger a little

---
+++ src/main/java/com/better/alarm/logger/StartupLogWriter.kt
@@ -0,0 +1,46 @@
+package com.better.alarm.logger
+
+import com.better.alarm.logger.Logger.LogLevel
+import com.better.alarm.logger.Logger.LogWriter
+import java.text.DateFormat
+import java.text.SimpleDateFormat
+import java.util.*
+import java.util.concurrent.locks.ReentrantLock
+import kotlin.collections.ArrayList
+
+class StartupLogWriter private constructor() : LogWriter {
+    private val bufferSize = 500
+    private val dtf: DateFormat = SimpleDateFormat("dd-MM HH:mm:ss", Locale.GERMANY)
+    /**
+     * guarded by [lock], because of the ContentProvider
+     */
+    private val messages = ArrayList<String>(100)
+    private val lock = ReentrantLock()
+
+    fun getMessagesAsString(): String {
+        lock.lock()
+        val ret = messages.joinToString("\r\n")
+        lock.unlock()
+        return ret
+    }
+
+    override fun write(level: LogLevel, tag: String, message: String, throwable: Throwable?) {
+        val timeStamp = Date(System.currentTimeMillis())
+        val exceptionMessage = throwable?.message ?: ""
+        val message = "${dtf.format(timeStamp)} ${level.name} $tag $message $exceptionMessage"
+
+        lock.lock()
+        if (messages.size == bufferSize) {
+            messages.removeAt(0)
+        }
+        messages.add(message)
+        lock.unlock()
+    }
+
+    companion object {
+        @JvmStatic
+        fun create(): StartupLogWriter {
+            return StartupLogWriter()
+        }
+    }
+}
