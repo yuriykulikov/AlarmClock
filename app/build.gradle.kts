f24e8a7794d4f8b0d8cbd6f5338d0a3554334083
Yuriy Kulikov <yuriy.kulikov.87@gmail.com>
2019-04-22 23:41:29 +0200

    Gradle KTS and jacocoTestReport

---
+++ app/build.gradle.kts
@@ -0,0 +1,123 @@
+buildscript {
+    dependencies {
+        classpath(kotlin("gradle-plugin", version = "1.3.30"))
+    }
+}
+
+plugins {
+    id("com.android.application")
+    kotlin("android")
+    jacoco
+}
+
+jacoco {
+    toolVersion = "0.8.3"
+}
+
+// ./gradlew test connectedDevelopDebugAndroidTest jacocoTestReport
+// task must be created, examples in Kotlin which call tasks.jacocoTestReport do not work
+tasks.create("jacocoTestReport", JacocoReport::class.java) {
+    group = "Reporting"
+    description = "Generate Jacoco coverage reports."
+
+    reports {
+        xml.isEnabled = true
+        html.isEnabled = true
+    }
+
+    val fileFilter = listOf("**/R.class", "**/R$*.class", "**/BuildConfig.*", "**/Manifest*.*", "**/*Test*.*", "android/**/*.*")
+
+    val flavorAndBuildType = "developDebug"
+
+    sourceDirectories.setFrom(files(listOf(
+            "$projectDir/src/main/java",
+            "$projectDir/src/main/kotlin"
+    )))
+    classDirectories.setFrom(files(listOf(
+            fileTree("dir" to "$buildDir/intermediates/javac/$flavorAndBuildType", "excludes" to fileFilter),
+            fileTree("dir" to "$buildDir/tmp/kotlin-classes/$flavorAndBuildType", "excludes" to fileFilter)
+    )))
+
+    // execution data from both unit and instrumentation tests
+    executionData.setFrom(fileTree(
+            "dir" to project.buildDir,
+            "includes" to listOf(
+                    // unit tests
+                    "jacoco/test${flavorAndBuildType.capitalize()}UnitTest.exec",
+                    // instrumentation tests
+                    "outputs/code_coverage/${flavorAndBuildType}AndroidTest/connected/**/*.ec"
+            )
+    ))
+
+    dependsOn("test")
+    dependsOn("connected${flavorAndBuildType.capitalize()}AndroidTest")
+}
+
+tasks.withType(Test::class.java) {
+    (this.extensions.getByName("jacoco") as JacocoTaskExtension).isIncludeNoLocationClasses = true
+}
+
+android {
+    compileSdkVersion(26)
+    defaultConfig {
+        applicationId = "com.better.alarm"
+        minSdkVersion(16)
+        targetSdkVersion(26)
+        testApplicationId = "com.better.alarm.test"
+        testInstrumentationRunner = "android.support.test.runner.AndroidJUnitRunner"
+    }
+    buildTypes {
+        getByName("debug") {
+            isTestCoverageEnabled = true
+        }
+        getByName("release") {
+            isMinifyEnabled = false
+            proguardFiles(getDefaultProguardFile("proguard-android.txt"), "proguard-rules.txt")
+        }
+    }
+    flavorDimensions("default")
+    productFlavors {
+        create("develop") {
+            applicationId = "com.better.alarm"
+        }
+        create("premium") {
+            applicationId = "com.premium.alarm"
+        }
+    }
+
+    lintOptions {
+        isAbortOnError = false
+    }
+
+    adbOptions {
+        timeOutInMs = 20 * 60 * 1000  // 20 minutes
+        installOptions("-d", "-t")
+    }
+}
+
+dependencies {
+    // App dependencies
+    implementation(kotlin("stdlib", version = "1.3.30"))
+    implementation("com.android.support:support-annotations:28.0.0")
+    implementation("ch.acra:acra:4.6.1")
+    implementation("com.melnykov:floatingactionbutton:1.2.0")
+    implementation("io.reactivex.rxjava2:rxjava:2.2.0")
+    implementation("io.reactivex.rxjava2:rxandroid:2.1.0")
+    implementation("com.f2prateek.rx.preferences2:rx-preferences:2.0.0")
+    implementation("com.android.support:support-v4:26.1.0") {}
+
+    // Testing-only dependencies
+    testImplementation("net.wuerl.kotlin:assertj-core-kotlin:0.1.1")
+    testImplementation("junit:junit:4.12")
+    testImplementation("org.mockito:mockito-core:2.23.4")
+
+    androidTestImplementation("com.squareup.assertj:assertj-android:1.1.1")
+    // Force usage of support annotations in the test app, since it is internally used by the runner module.
+    androidTestImplementation("com.android.support:support-annotations:28.0.0")
+    androidTestImplementation("com.android.support.test:runner:1.0.2") {}
+    androidTestImplementation("com.android.support.test:rules:1.0.2") {}
+    androidTestImplementation("com.android.support.test.espresso:espresso-core:3.0.2")
+    androidTestImplementation("com.bartoszlipinski:cortado:1.2.0")
+    //for some tests which do not work with espresso on travis
+    androidTestImplementation("com.jayway.android.robotium:robotium-solo:5.2.1")
+}
