yuriykulikov/AlarmClock

commit dce0f6baf7980eac80590208d59149e6a4e1ad88

Fixed 12:30PM missing OK button

app/src/main/AndroidManifest.xml
@@ -1,7 +1,7 @@
 <manifest xmlns:android="http://schemas.android.com/apk/res/android"
     package="com.better.alarm"
-    android:versionCode="30203"
-    android:versionName="3.02.03">
+    android:versionCode="30205"
+    android:versionName="3.02.05">
 
     <uses-permission android:name="android.permission.RECEIVE_BOOT_COMPLETED" />
     <uses-permission android:name="android.permission.WAKE_LOCK" />

app/src/main/java/com/better/alarm/view/TimePickerPresenter.kt
@@ -2,6 +2,7 @@ package com.better.alarm.view
 
 import io.reactivex.Observable
 import io.reactivex.subjects.BehaviorSubject
+import java.util.*
 
 /**
  * Created by Yuriy on 17.08.2017.
@@ -90,15 +91,19 @@ open class TimePickerPresenter(val is24HoursMode: Boolean) {
         val minutesTensDigit: Int by lazy { inverted.getOrElse(1, { -1 }) }
         val minutesOnesDigit: Int by lazy { inverted.getOrElse(0, { -1 }) }
 
-        val hours: Int by lazy {
-            inverted.getOrElse(3, { 0 }) * 10 + hoursOnesDigit + if (amPm == AmPm.PM) 12 else 0
-        }
-
         val minutes: Int  by lazy { minutesTensDigit * 10 + minutesOnesDigit }
+        val hours: Int by lazy {
+            Calendar.getInstance().apply {
+                set(Calendar.HOUR, inverted.getOrElse(3, { 0 }) * 10 + hoursOnesDigit)
+                set(Calendar.MINUTE, minutes)
+                if (amPm == AmPm.PM) set(Calendar.AM_PM, Calendar.PM)
+                if (amPm == AmPm.AM) set(Calendar.AM_PM, Calendar.AM)
+            }.get(Calendar.HOUR_OF_DAY)
+        }
 
         fun isTimeValid(): Boolean = when {
             is24HoursMode -> inverted.size >= 3 && hours <= 23 && minutes <= 60
-            else -> inverted.size >= 3 && hours <= 23 && minutes <= 60 && leftRightEntered
+            else -> inverted.size >= 3 && minutes <= 60 && leftRightEntered
         }
 
         private fun front(): Int = input.getOrElse(0, { 0 }) * 10 + input.getOrElse(1, { 0 })
@@ -139,7 +144,7 @@ open class TimePickerPresenter(val is24HoursMode: Boolean) {
 
         private fun enable12Digits(): List<Key> = when {
             leftRightEntered -> none
-            input.isEmpty() -> any
+            input.isEmpty() -> any.minus(Key.ZERO)
             //second number can be tens of hours or tens of minutes
             input.size == 1 -> zeroToFive
             //third number can tens of minutes or minutes (any)

app/src/test/java/com/better/alarm/TimePickerPresenterTest.kt
@@ -124,6 +124,52 @@ class TimePickerPresenterTest {
     }
 
     @Test
+    fun when1239pm() {
+        val presenter = TimePickerPresenter(false)
+
+        presenter.onClick(Key.ONE)
+        presenter.onClick(Key.TWO)
+        presenter.onClick(Key.THREE)
+        presenter.onClick(Key.FIVE)
+        presenter.onClick(Key.RIGHT)
+
+        val last = presenter.state.test().values().last()
+
+        System.out.println(last)
+
+        assertThat(last.hoursTensDigit).isEqualTo(1)
+        assertThat(last.hoursOnesDigit).isEqualTo(2)
+        assertThat(last.minutesTensDigit).isEqualTo(3)
+        assertThat(last.minutesOnesDigit).isEqualTo(5)
+        assertThat(last.amPm).isEqualTo(TimePickerPresenter.AmPm.PM)
+        assertThat(last.hours).isEqualTo(0)
+        assertThat(last.minutes).isEqualTo(35)
+    }
+
+    @Test
+    fun when1239am() {
+        val presenter = TimePickerPresenter(false)
+
+        presenter.onClick(Key.ONE)
+        presenter.onClick(Key.TWO)
+        presenter.onClick(Key.THREE)
+        presenter.onClick(Key.FIVE)
+        presenter.onClick(Key.LEFT)
+
+        val last = presenter.state.test().values().last()
+
+        System.out.println(last)
+
+        assertThat(last.hoursTensDigit).isEqualTo(1)
+        assertThat(last.hoursOnesDigit).isEqualTo(2)
+        assertThat(last.minutesTensDigit).isEqualTo(3)
+        assertThat(last.minutesOnesDigit).isEqualTo(5)
+        assertThat(last.amPm).isEqualTo(TimePickerPresenter.AmPm.AM)
+        assertThat(last.hours).isEqualTo(12)
+        assertThat(last.minutes).isEqualTo(35)
+    }
+
+    @Test
     fun ok() {
         val presenter = TimePickerPresenter(false)
 

app/src/androidTest/java/com/better/alarm/test/PickerAmTest.kt
@@ -1,0 +1,195 @@
+package com.better.alarm.test
+
+import android.support.test.espresso.Espresso
+import android.support.test.espresso.ViewInteraction
+import android.support.test.espresso.action.ViewActions
+import android.support.test.espresso.assertion.ViewAssertions.matches
+import android.support.test.espresso.matcher.ViewMatchers
+import android.support.test.espresso.matcher.ViewMatchers.isEnabled
+import android.support.test.rule.ActivityTestRule
+import android.support.test.runner.AndroidJUnit4
+import android.view.View
+import com.better.alarm.R
+import com.better.alarm.configuration.AlarmApplication
+import com.better.alarm.presenter.AlarmsListActivity
+import com.better.alarm.util.Optional
+import org.hamcrest.Matchers
+import org.hamcrest.Matchers.not
+import org.junit.After
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+import org.junit.rules.RuleChain
+import org.junit.rules.TestRule
+import org.junit.runner.RunWith
+import java.util.*
+
+@RunWith(AndroidJUnit4::class)
+class PickerAmTest : BaseTest() {
+    @JvmField
+    var listActivity = ActivityTestRule(
+            AlarmsListActivity::class.java, false, /* autostart*/ true)
+
+    @JvmField
+    @Rule
+    var chain: TestRule = RuleChain.outerRule(ForceLocaleRule(Locale.US)).around(listActivity)
+
+    class OnView {
+        fun with(id: Int? = null, text: String? = null): ViewInteraction {
+            val matcher = listOfNotNull(
+                    text?.let { ViewMatchers.withText(it) },
+                    id?.let { ViewMatchers.withId(it) }
+            ).let { Matchers.allOf(it) }
+            return Espresso.onView(matcher)
+        }
+    }
+
+    fun onView(): OnView {
+        return OnView()
+    }
+
+    fun ViewInteraction.click() {
+        perform(ViewActions.click())
+    }
+
+    fun ViewInteraction.assertDisabled() {
+        check(matches(not<View>(isEnabled())))
+    }
+
+    fun ViewInteraction.assertEnabled() {
+        check(matches(isEnabled()))
+    }
+
+    @Before
+    fun setUp() {
+        AlarmApplication.is24hoursFormatOverride = Optional.of(false)
+        onView().with(id = R.id.fab).click()
+        //sleep();
+    }
+
+    @After
+    override fun tearDown() {
+        onView().with(text = "Cancel").click()
+        //sleep();
+        onView().with(text = "Cancel").click()
+        assertThatList().items().hasSize(2)
+    }
+
+    @Test
+    fun testNothing() {
+        assertTimerView("--:--")
+        arrayOf(one(), two(), three(), four(), five(), six(), seven(), eight(), nine()).forEach { it.assertEnabled() }
+        zero().assertDisabled()
+        ok().assertDisabled()
+        pm().assertDisabled()
+    }
+
+    @Test
+    fun test1() {
+        one().click()
+        arrayOf(zero(), one(), two()).forEach { it.assertEnabled() }
+        ok().assertDisabled()
+        pm().assertDisabled()
+    }
+
+    @Test
+    fun test2() {
+        two().click()
+        arrayOf(zero(), one(), two(), three(), four(), five()).forEach { it.assertEnabled() }
+        arrayOf(six(), seven(), eight(), nine()).forEach { it.assertDisabled() }
+        ok().assertDisabled()
+        pm().assertDisabled()
+    }
+
+    @Test
+    fun test3() {
+        three().click()
+        arrayOf(zero(), one(), two(), three(), four(), five()).forEach { it.assertEnabled() }
+        arrayOf(six(), seven(), eight(), nine()).forEach { it.assertDisabled() }
+        ok().assertDisabled()
+        pm().assertDisabled()
+    }
+
+    @Test
+    fun test4() {
+        four().click()
+        arrayOf(zero(), one(), two(), three(), four(), five()).forEach { it.assertEnabled() }
+        arrayOf(six(), seven(), eight(), nine()).forEach { it.assertDisabled() }
+        ok().assertDisabled()
+        pm().assertDisabled()
+    }
+
+    @Test
+    fun test5() {
+        five().click()
+        arrayOf(zero(), one(), two(), three(), four(), five()).forEach { it.assertEnabled() }
+        arrayOf(six(), seven(), eight(), nine()).forEach { it.assertDisabled() }
+        ok().assertDisabled()
+        pm().assertDisabled()
+    }
+
+    @Test
+    fun test6() {
+        six().click()
+        arrayOf(zero(), one(), two(), three(), four(), five()).forEach { it.assertEnabled() }
+        arrayOf(six(), seven(), eight(), nine()).forEach { it.assertDisabled() }
+        ok().assertDisabled()
+        pm().assertDisabled()
+    }
+
+    @Test
+    fun test7() {
+        seven().click()
+        arrayOf(zero(), one(), two(), three(), four(), five()).forEach { it.assertEnabled() }
+        arrayOf(six(), seven(), eight(), nine()).forEach { it.assertDisabled() }
+        ok().assertDisabled()
+        pm().assertDisabled()
+    }
+
+    @Test
+    fun test8() {
+        eight().click()
+        arrayOf(zero(), one(), two(), three(), four(), five()).forEach { it.assertEnabled() }
+        arrayOf(six(), seven(), eight(), nine()).forEach { it.assertDisabled() }
+        ok().assertDisabled()
+        pm().assertDisabled()
+    }
+
+    @Test
+    fun test9() {
+        nine().click()
+        arrayOf(zero(), one(), two(), three(), four(), five()).forEach { it.assertEnabled() }
+        arrayOf(six(), seven(), eight(), nine()).forEach { it.assertDisabled() }
+        ok().assertDisabled()
+        pm().assertDisabled()
+    }
+
+    @Test
+    fun test1230pm() {
+        assert(one() == one())
+
+        one().click()
+        two().click()
+        three().click()
+        zero().click()
+        pm().click()
+
+        arrayOf<ViewInteraction>().forEach { it.assertEnabled() }
+        pm().assertDisabled()
+        ok().assertEnabled()
+    }
+
+    private fun one(): ViewInteraction = onView().with(id = R.id.key_left, text = "1")
+    private fun two(): ViewInteraction = onView().with(id = R.id.key_middle, text = "2")
+    private fun three(): ViewInteraction = onView().with(id = R.id.key_right, text = "3")
+    private fun four(): ViewInteraction = onView().with(id = R.id.key_left, text = "4")
+    private fun five(): ViewInteraction = onView().with(id = R.id.key_middle, text = "5")
+    private fun six(): ViewInteraction = onView().with(id = R.id.key_right, text = "6")
+    private fun seven(): ViewInteraction = onView().with(id = R.id.key_left, text = "7")
+    private fun eight(): ViewInteraction = onView().with(id = R.id.key_middle, text = "8")
+    private fun nine(): ViewInteraction = onView().with(id = R.id.key_right, text = "9")
+    private fun zero(): ViewInteraction = onView().with(id = R.id.key_middle, text = "0")
+    private fun ok(): ViewInteraction = onView().with(text = "OK")
+    private fun am(): ViewInteraction = onView().with(id = R.id.key_left, text = "AM")
+    private fun pm(): ViewInteraction = onView().with(id = R.id.key_right, text = "PM")
+}

commit e0745c3896d80429c1dd1876c61ae9c28aab3bca

Fixed a regression caused by dce0f6baf7980eac80590208d59149e6a4e1ad88

app/src/main/AndroidManifest.xml
@@ -1,7 +1,7 @@
 <manifest xmlns:android="http://schemas.android.com/apk/res/android"
     package="com.better.alarm"
-    android:versionCode="30205"
-    android:versionName="3.02.05">
+    android:versionCode="30206"
+    android:versionName="3.02.06">
 
     <uses-permission android:name="android.permission.RECEIVE_BOOT_COMPLETED" />
     <uses-permission android:name="android.permission.WAKE_LOCK" />

app/src/main/java/com/better/alarm/view/TimePickerPresenter.kt
@@ -2,7 +2,6 @@ package com.better.alarm.view
 
 import io.reactivex.Observable
 import io.reactivex.subjects.BehaviorSubject
-import java.util.*
 
 /**
  * Created by Yuriy on 17.08.2017.
@@ -93,12 +92,14 @@ open class TimePickerPresenter(val is24HoursMode: Boolean) {
 
         val minutes: Int  by lazy { minutesTensDigit * 10 + minutesOnesDigit }
         val hours: Int by lazy {
-            Calendar.getInstance().apply {
-                set(Calendar.HOUR, inverted.getOrElse(3, { 0 }) * 10 + hoursOnesDigit)
-                set(Calendar.MINUTE, minutes)
-                if (amPm == AmPm.PM) set(Calendar.AM_PM, Calendar.PM)
-                if (amPm == AmPm.AM) set(Calendar.AM_PM, Calendar.AM)
-            }.get(Calendar.HOUR_OF_DAY)
+            val hours = inverted.getOrElse(3, { 0 }) * 10 + hoursOnesDigit
+            when {
+                is24HoursMode -> hours
+                hours == 12 && amPm == AmPm.AM -> 0
+                hours == 12 && amPm == AmPm.PM -> 12
+                amPm == AmPm.PM -> hours + 12
+                else -> hours
+            }
         }
 
         fun isTimeValid(): Boolean = when {

app/src/test/java/com/better/alarm/TimePickerPresenterTest.kt
@@ -23,13 +23,53 @@ class TimePickerPresenterTest {
     }
 
     @Test
-    fun enter4digits() {
+    fun enter4digits24() {
+        val presenter = TimePickerPresenter(true)
+
+        presenter.onClick(Key.ONE)
+        presenter.onClick(Key.TWO)
+        presenter.onClick(Key.THREE)
+        presenter.onClick(Key.FIVE)
+
+        val last = presenter.state.test().values().last()
+
+        assertThat(last.hoursTensDigit).isEqualTo(1)
+        assertThat(last.hoursOnesDigit).isEqualTo(2)
+        assertThat(last.minutesTensDigit).isEqualTo(3)
+        assertThat(last.minutesOnesDigit).isEqualTo(5)
+        assertThat(last.hours).isEqualTo(12)
+        assertThat(last.minutes).isEqualTo(35)
+    }
+
+    @Test
+    fun enter4digitsAm() {
         val presenter = TimePickerPresenter(false)
 
         presenter.onClick(Key.ONE)
         presenter.onClick(Key.TWO)
         presenter.onClick(Key.THREE)
         presenter.onClick(Key.FIVE)
+        presenter.onClick(Key.LEFT)
+
+        val last = presenter.state.test().values().last()
+
+        assertThat(last.hoursTensDigit).isEqualTo(1)
+        assertThat(last.hoursOnesDigit).isEqualTo(2)
+        assertThat(last.minutesTensDigit).isEqualTo(3)
+        assertThat(last.minutesOnesDigit).isEqualTo(5)
+        assertThat(last.hours).isEqualTo(0)
+        assertThat(last.minutes).isEqualTo(35)
+    }
+
+    @Test
+    fun enter4digitsPm() {
+        val presenter = TimePickerPresenter(false)
+
+        presenter.onClick(Key.ONE)
+        presenter.onClick(Key.TWO)
+        presenter.onClick(Key.THREE)
+        presenter.onClick(Key.FIVE)
+        presenter.onClick(Key.RIGHT)
 
         val last = presenter.state.test().values().last()
 
@@ -142,7 +182,7 @@ class TimePickerPresenterTest {
         assertThat(last.minutesTensDigit).isEqualTo(3)
         assertThat(last.minutesOnesDigit).isEqualTo(5)
         assertThat(last.amPm).isEqualTo(TimePickerPresenter.AmPm.PM)
-        assertThat(last.hours).isEqualTo(0)
+        assertThat(last.hours).isEqualTo(12)
         assertThat(last.minutes).isEqualTo(35)
     }
 
@@ -165,8 +205,30 @@ class TimePickerPresenterTest {
         assertThat(last.minutesTensDigit).isEqualTo(3)
         assertThat(last.minutesOnesDigit).isEqualTo(5)
         assertThat(last.amPm).isEqualTo(TimePickerPresenter.AmPm.AM)
-        assertThat(last.hours).isEqualTo(12)
+        assertThat(last.hours).isEqualTo(0)
         assertThat(last.minutes).isEqualTo(35)
+    }
+
+    @Test
+    fun when1605() {
+        val presenter = TimePickerPresenter(true)
+
+        presenter.onClick(Key.ONE)
+        presenter.onClick(Key.SIX)
+        presenter.onClick(Key.ZERO)
+        presenter.onClick(Key.FIVE)
+
+        val last = presenter.state.test().values().last()
+
+        System.out.println(last)
+
+        assertThat(last.hoursTensDigit).isEqualTo(1)
+        assertThat(last.hoursOnesDigit).isEqualTo(6)
+        assertThat(last.minutesTensDigit).isEqualTo(0)
+        assertThat(last.minutesOnesDigit).isEqualTo(5)
+        assertThat(last.amPm).isEqualTo(TimePickerPresenter.AmPm.NONE)
+        assertThat(last.hours).isEqualTo(16)
+        assertThat(last.minutes).isEqualTo(5)
     }
 
     @Test