c786d7dddee6ad85645904e7e585c99e45579daa
Yuriy Kulikov <yuriy.kulikov.87@gmail.com>
2019-08-11 02:16:35 +0200

    Themes: Synthwave

--- app/src/main/java/com/better/alarm/presenter/DynamicThemeHandler.kt
+++ app/src/main/java/com/better/alarm/presenter/DynamicThemeHandler.kt
@@ -10,29 +10,36 @@ class DynamicThemeHandler(context: Context) {
     private val themeKey = "theme"
     private val sp: SharedPreferences = PreferenceManager.getDefaultSharedPreferences(context)
 
+    private val synthwave = "synthwave"
+    private val light = "light"
+    private val dark = "dark"
+
     init {
-        when (sp.getString(themeKey, "dark")) {
-            "light", "dark" -> {
+        when (sp.getString(themeKey, dark)) {
+            light, dark, synthwave -> {
             }
             else -> {
-                sp.edit().putString(themeKey, "dark").apply()
+                sp.edit().putString(themeKey, dark).apply()
             }
         }
     }
 
     fun defaultTheme(): Int = when (preference()) {
-        "light" -> R.style.DefaultLightTheme
-        "dark" -> R.style.DefaultDarkTheme
+        light -> R.style.DefaultLightTheme
+        dark -> R.style.DefaultDarkTheme
+        synthwave -> R.style.DefaultSynthwaveTheme
         else -> R.style.DefaultDarkTheme
     }
 
-    private fun preference(): String = sp.getString(themeKey, "dark")
+    private fun preference(): String = sp.getString(themeKey, dark)
 
     fun getIdForName(name: String): Int = when {
-        preference() == "light" && name == AlarmAlertFullScreen::class.java.name -> R.style.AlarmAlertFullScreenLightTheme
-        preference() == "light" && name == TimePickerDialogFragment::class.java.name -> R.style.TimePickerDialogFragmentLight
-        preference() == "dark" && name == AlarmAlertFullScreen::class.java.name -> R.style.AlarmAlertFullScreenDarkTheme
-        preference() == "dark" && name == TimePickerDialogFragment::class.java.name -> R.style.TimePickerDialogFragmentDark
+        preference() == light && name == AlarmAlertFullScreen::class.java.name -> R.style.AlarmAlertFullScreenLightTheme
+        preference() == light && name == TimePickerDialogFragment::class.java.name -> R.style.TimePickerDialogFragmentLight
+        preference() == dark && name == AlarmAlertFullScreen::class.java.name -> R.style.AlarmAlertFullScreenDarkTheme
+        preference() == dark && name == TimePickerDialogFragment::class.java.name -> R.style.TimePickerDialogFragmentDark
+        preference() == synthwave && name == AlarmAlertFullScreen::class.java.name -> R.style.AlarmAlertFullScreenSynthwaveTheme
+        preference() == synthwave && name == TimePickerDialogFragment::class.java.name -> R.style.TimePickerDialogFragmentSynthwave
         else -> defaultTheme()
     }
 }
