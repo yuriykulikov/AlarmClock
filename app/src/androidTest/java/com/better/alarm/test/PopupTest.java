b39efc842aac557e4ba55e55f725bdf29d0d4cb2
Yuriy Kulikov <yuriy.kulikov.87@gmail.com>
2021-08-21 14:51:57 +0200

    Reverted the Popup test

--- app/src/androidTest/java/com/better/alarm/test/PopupTest.java
+++ app/src/androidTest/java/com/better/alarm/test/PopupTest.java
@@ -2,21 +2,25 @@ package com.better.alarm.test;
 
 import static android.provider.AlarmClock.ACTION_SET_ALARM;
 import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.action.ViewActions.click;
 import static androidx.test.espresso.action.ViewActions.longClick;
+import static androidx.test.espresso.matcher.ViewMatchers.isChecked;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
 import static androidx.test.espresso.matcher.ViewMatchers.withText;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.hamcrest.Matchers.allOf;
 
-import android.content.Context;
 import android.content.Intent;
 import android.provider.AlarmClock;
 
-import androidx.test.core.app.ActivityScenario;
-import androidx.test.ext.junit.rules.ActivityScenarioRule;
-import androidx.test.ext.junit.runners.AndroidJUnit4;
-import androidx.test.platform.app.InstrumentationRegistry;
+import androidx.test.rule.ActivityTestRule;
+import androidx.test.runner.AndroidJUnit4;
 
 import com.better.alarm.R;
 import com.better.alarm.alert.AlarmAlertFullScreen;
 import com.better.alarm.configuration.ContainerKt;
+import com.better.alarm.configuration.InjectKt;
+import com.better.alarm.configuration.Store;
 import com.better.alarm.interfaces.Intents;
 import com.better.alarm.model.AlarmSetter;
 import com.better.alarm.model.AlarmValue;
@@ -24,14 +28,18 @@ import com.better.alarm.model.AlarmsReceiver;
 import com.better.alarm.model.CalendarType;
 import com.better.alarm.presenter.AlarmsListActivity;
 import com.better.alarm.presenter.HandleSetAlarm;
+import com.better.alarm.presenter.TransparentActivity;
+import com.better.alarm.util.Optional;
 
 import org.junit.Before;
+import org.junit.Ignore;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.RuleChain;
 import org.junit.rules.TestRule;
 import org.junit.runner.RunWith;
 
+import java.util.Calendar;
 import java.util.Locale;
 
 /**
@@ -39,16 +47,19 @@ import java.util.Locale;
  */
 @RunWith(AndroidJUnit4.class)
 public class PopupTest extends BaseTest {
-    private Context getContext() {
-        return InstrumentationRegistry.getInstrumentation().getTargetContext();
-    }
-
-    public ActivityScenarioRule<AlarmsListActivity> listActivity = new ActivityScenarioRule<>(AlarmsListActivity.class);
+    public ActivityTestRule<AlarmAlertFullScreen> alertActivity = new ActivityTestRule<AlarmAlertFullScreen>(
+            AlarmAlertFullScreen.class, false, /* autostart*/ false);
+    public ActivityTestRule<AlarmsListActivity> listActivity = new ActivityTestRule<AlarmsListActivity>(
+            AlarmsListActivity.class, false, /* autostart*/ true);
+    public ActivityTestRule<TransparentActivity> transparentActivity = new ActivityTestRule<TransparentActivity>(
+            TransparentActivity.class, false, /* autostart*/ false);
 
     @Rule
     public TestRule chain = RuleChain
             .outerRule(new ForceLocaleRule(Locale.US))
-            .around(listActivity);
+            .around(listActivity)
+            .around(alertActivity)
+            .around(transparentActivity);
 
     @Before
     public void set24HourMode() {
@@ -56,20 +67,17 @@ public class PopupTest extends BaseTest {
     }
 
     public int createAlarmAndFire() {
-        listActivity.getScenario().onActivity(activity -> {
-            Intent createAlarm = new Intent();
-            createAlarm.setClass(activity, HandleSetAlarm.class);
-            createAlarm.setAction(ACTION_SET_ALARM);
-            createAlarm.putExtra(AlarmClock.EXTRA_SKIP_UI, true);
-            createAlarm.putExtra(AlarmClock.EXTRA_HOUR, 0);
-            createAlarm.putExtra(AlarmClock.EXTRA_MINUTES, 0);
-            createAlarm.putExtra(AlarmClock.EXTRA_MESSAGE, "From outside");
-            activity.startActivity(createAlarm);
-        });
+        Intent createAlarm = new Intent();
+        createAlarm.setClass(listActivity.getActivity(), HandleSetAlarm.class);
+        createAlarm.setAction(ACTION_SET_ALARM);
+        createAlarm.putExtra(AlarmClock.EXTRA_SKIP_UI, true);
+        createAlarm.putExtra(AlarmClock.EXTRA_HOUR, 0);
+        createAlarm.putExtra(AlarmClock.EXTRA_MINUTES, 0);
+        createAlarm.putExtra(AlarmClock.EXTRA_MESSAGE, "From outside");
+
+        listActivity.getActivity().startActivity(createAlarm);
 
         sleep();
-
-        listActivity.getScenario().recreate();
 
         assertThatList()
                 .filter(enabled())
@@ -80,11 +88,11 @@ public class PopupTest extends BaseTest {
 
         //simulate alarm fired
         Intent intent = new Intent(AlarmSetter.ACTION_FIRED);
-        intent.setClass(getContext(), AlarmsReceiver.class);
+        intent.setClass(listActivity.getActivity(), AlarmsReceiver.class);
         intent.putExtra(AlarmSetter.EXTRA_ID, id);
         intent.putExtra(AlarmSetter.EXTRA_TYPE, CalendarType.NORMAL.name());
 
-        getContext().sendBroadcast(intent);
+        listActivity.getActivity().sendBroadcast(intent);
 
         sleep();
         return id;
@@ -94,9 +102,9 @@ public class PopupTest extends BaseTest {
     public void dismissViaAlarmAlert() {
         int id = createAlarmAndFire();
 
-        Intent startIntent = new Intent(getContext(), AlarmAlertFullScreen.class);
+        Intent startIntent = new Intent();
         startIntent.putExtra(Intents.EXTRA_ID, id);
-        ActivityScenario.launch(startIntent);
+        alertActivity.launchActivity(startIntent);
 
         sleep();
 
@@ -105,21 +113,91 @@ public class PopupTest extends BaseTest {
         deleteAlarm();
     }
 
+    private void afterLongClickSnoozeAlarmCheckAndDelete() {
+        assertTimerView("--:--");
+        onView(withText("2")).perform(click());
+        assertTimerView("--:-2");
+        onView(withText("3")).perform(click());
+        assertTimerView("--:23");
+        onView(withText("5")).perform(click());
+        assertTimerView("-2:35");
+        onView(withText("9")).perform(click());
+        assertTimerView("23:59");
+        sleep();
+
+        onView(withText("OK")).perform(click());
+
+        sleep();
+
+        Optional<Store.Next> next = InjectKt.globalInject(Store.class).getValue()
+                .next()
+                .blockingFirst();
+
+
+        assertThat(next.isPresent()).isTrue();
+
+        Calendar nextTime = Calendar.getInstance();
+        nextTime.setTimeInMillis(next.get().nextNonPrealarmTime());
+
+        assertThat((int) nextTime.get(Calendar.HOUR_OF_DAY)).isEqualTo(23);
+        assertThat((int) nextTime.get(Calendar.MINUTE)).isEqualTo(59);
+        assertThat(next.get().alarm().isEnabled()).isTrue();
+
+        //disable the snoozed alarm
+        onView(allOf(withId(R.id.list_row_on_off_switch), isChecked())).perform(click());
+
+        sleep();
+
+        assertThatList()
+                .filter(enabled())
+                .items()
+                .isEmpty();
+
+        deleteAlarm();
+    }
+
+    @Ignore("Flakey")
+    @Test
+    public void snoozeViaClick() {
+        int id = createAlarmAndFire();
+
+        Intent startIntent = new Intent();
+        startIntent.putExtra(Intents.EXTRA_ID, id);
+        alertActivity.launchActivity(startIntent);
+
+        sleep();
+        onView(withText("Snooze")).perform(click());
+
+        deleteAlarm();
+    }
+
+    @Ignore
+    @Test
+    public void snoozeViaNotificationPicker() {
+        int id = createAlarmAndFire();
+
+        Intent startIntent = new Intent();
+        startIntent.putExtra(Intents.EXTRA_ID, id);
+        transparentActivity.launchActivity(startIntent);
+
+        deleteAlarm();
+    }
+
     @Test
     public void letAlarmExpireAndDismissIt() throws InterruptedException {
         int id = createAlarmAndFire();
 
-        Intent startIntent = new Intent(getContext(), AlarmAlertFullScreen.class);
+        Intent startIntent = new Intent();
         startIntent.putExtra(Intents.EXTRA_ID, id);
-        ActivityScenario.launch(startIntent);
+        alertActivity.launchActivity(startIntent);
 
         //simulate timed out
         Intent intent = new Intent(AlarmSetter.ACTION_FIRED);
-        intent.setClass(getContext(), AlarmsReceiver.class);
+        intent.setClass(alertActivity.getActivity(), AlarmsReceiver.class);
         intent.putExtra(AlarmSetter.EXTRA_ID, id);
         intent.putExtra(AlarmSetter.EXTRA_TYPE, CalendarType.AUTOSILENCE.name());
 
-        getContext().sendBroadcast(intent);
+        listActivity.getActivity().sendBroadcast(intent);
         sleep();
 
         //Popup must be closed in order for this to work
@@ -130,12 +208,7 @@ public class PopupTest extends BaseTest {
 
     private void deleteAlarm() {
         sleep();
-        ActivityScenario.launch(AlarmsListActivity.class);
         deleteAlarm(0);
         assertThatList().items().hasSize(2);
-    }
-
-    protected static void sleep() {
-
     }
 }
