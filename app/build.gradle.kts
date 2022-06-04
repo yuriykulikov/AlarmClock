3b4bec0b7ebb3ff2684941af0ac252768f2a853b
Yuriy Kulikov <yuriy.kulikov.87@gmail.com>
2022-06-04 23:17:47 +0200

    localization(ukranian): imported ukrainian translation

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -72,8 +72,8 @@ val acraEmail =
 android {
   compileSdk = 31
   defaultConfig {
-    versionCode = 31008
-    versionName = "3.10.08"
+    versionCode = 31009
+    versionName = "3.10.09"
     applicationId = "com.better.alarm"
     minSdk = 16
     targetSdk = 31
