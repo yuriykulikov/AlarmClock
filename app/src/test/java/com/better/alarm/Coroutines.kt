6e02096575c6e20c3ae9099fe119f5cd77ae6eed
Yuriy Kulikov <yuriy.kulikov.87@gmail.com>
2022-11-27 15:08:44 +0100

    build: target SDK 33

--- app/src/test/java/com/better/alarm/Coroutines.kt
+++ app/src/test/java/com/better/alarm/Coroutines.kt
@@ -1,8 +1,10 @@
 package com.better.alarm
 
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.test.setMain
 
+@OptIn(ExperimentalCoroutinesApi::class)
 fun setMainUnconfined() {
   Dispatchers.setMain(Dispatchers.Unconfined)
 }
