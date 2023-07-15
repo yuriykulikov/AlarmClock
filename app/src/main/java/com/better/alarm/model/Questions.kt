66d413c3ccfb1b22cdd8bc5234e5110b3f836881
Jieun Chon <jieunchon@Jieuns-MacBook-Air.local>
2023-07-15 17:34:16 +0900

    Add Questions

--- app/src/main/java/com/better/alarm/model/Questions.kt
+++ app/src/main/java/com/better/alarm/model/Questions.kt
@@ -1,12 +1,3 @@
 package com.better.alarm.model
 
-import kotlinx.serialization.Serializable
 
-@Serializable
-data class Questions(
-    val id: Int,
-    val title: String,
-    val description: String,
-    val choices: List<String>,
-    val correctAnswer: Int
-)
