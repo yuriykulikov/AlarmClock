a16625c648b45449819b5ec677614835b16c44c1
Yuriy Kulikov <yuriy.kulikov.87@gmail.com>
2023-08-06 19:10:05 +0200

    refactor: moved AlarmsListAdapter to a new file

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -161,6 +161,7 @@ dependencies {
 
   implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:$coroutinesVersion")
   implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:$coroutinesVersion")
+  implementation("org.jetbrains.kotlinx:kotlinx-coroutines-rx2:$coroutinesVersion")
   implementation("com.google.android.material:material:1.8.0")
   implementation("org.slf4j:slf4j-api:1.7.36")
   implementation("com.github.tony19:logback-android:2.0.1")
