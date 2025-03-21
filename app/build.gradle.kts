6c552b2ea091402cca52768da8775987d6191766
andy <andy16830@gmail.com>
2025-03-21 12:46:30 +0800

    import yolo implementation by Surendra Maran and update dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -190,4 +190,14 @@ dependencies {
   androidTestImplementation("androidx.test:runner:$androidxTest")
   androidTestImplementation("androidx.test:rules:$androidxTest")
   androidTestImplementation("androidx.test.ext:junit:1.1.5")
+
+  implementation("org.tensorflow:tensorflow-lite:2.16.1")
+  implementation("org.tensorflow:tensorflow-lite-support:0.4.4")
+  implementation("org.tensorflow:tensorflow-lite-metadata:0.4.4")
+
+  implementation("org.tensorflow:tensorflow-lite-gpu-delegate-plugin:0.4.4")
+  implementation("org.tensorflow:tensorflow-lite-gpu-api:2.16.1")
+  implementation("org.tensorflow:tensorflow-lite-api:2.16.1")
+  implementation("org.tensorflow:tensorflow-lite-gpu:2.16.1")
+  implementation("org.tensorflow:tensorflow-lite-select-tf-ops:2.16.1")
 }
---
+++ app/src/main/java/com/better/alarm/vision/Constants.kt
@@ -0,0 +1,22 @@
+/*
+Creative Commons Attribution 4.0 International (CC BY 4.0)
+
+You are free to:
+
+- Share — copy and redistribute the material in any medium or format
+- Adapt — remix, transform, and build upon the material for any purpose, even commercially.
+
+Under the following terms:
+
+- Attribution — You must give appropriate credit, provide a link to the license, and indicate if changes were made. You may do so in any reasonable manner, but not in any way that suggests the licensor endorses you or your use.
+
+Credit must be given to: Surendra Maran (https://github.com/surendramaran)
+
+Full License Text: https://creativecommons.org/licenses/by/4.0/
+ */
+package com.better.alarm.vision
+
+object Constants {
+    const val MODEL_PATH = "head_float16.tflite"
+    val LABELS_PATH: String? = null // provide your labels.txt file if the metadata not present in the model
+}
