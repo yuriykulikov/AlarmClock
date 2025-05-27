8b63da454a1befaf9144b05b9532bcf5c7811950
andy <andy16830@gmail.com>
2025-05-27 20:10:46 +0800

    add "forever option" to skip duration settings

--- app/src/main/java/com/better/alarm/domain/AlarmCore.kt
+++ app/src/main/java/com/better/alarm/domain/AlarmCore.kt
@@ -411,6 +411,9 @@ class AlarmCore(
           skipTime == -1 -> {
             // switched off
           }
+          skipTime == -2 -> {
+            broadcastAlarmState(Intents.ALARM_SHOW_SKIP)
+          }
           toShowSkip.after(calendars.now()) -> {
             mAlarmsScheduler.setInexactAlarm(id, toShowSkip)
           }
